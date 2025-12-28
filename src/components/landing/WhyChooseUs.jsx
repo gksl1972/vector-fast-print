60a05de512e0b5a147a07e9461cea2d126e81de1
gksl rsy <gkslrsy1972@gmail.com>
2025-12-28 11:56:46 +0000

    File changes

--- src/components/landing/WhyChooseUs.jsx
+++ src/components/landing/WhyChooseUs.jsx
@@ -38,7 +38,7 @@ const trustPoints = [
 
 export default function WhyChooseUs() {
   return (
-    <section id="why-us" className="py-24 lg:py-32 bg-slate-50">
+    <section id="why-us" className="py-12 sm:py-16 lg:py-20 bg-slate-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Section Header */}
         <motion.div
